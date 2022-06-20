49a369a0a103dc1d45400ecbf1bd31c9f908341d
arshptl <arshpatel714@gmail.com>
2022-06-20 18:47:24 +0530

    RTL vs Normal testing

--- RTL/src/__tests__/react-dom.js
+++ RTL/src/__tests__/react-dom.js
@@ -1,10 +1,23 @@
+import '@testing-library/jest-dom'
 import * as React from 'react'
-import ReactDOM from 'react-dom'
-import {FavoriteNumber} from '../favorite-number'
+// import ReactDOM from 'react-dom'
+// import { getQueriesForElement } from '@testing-library/dom'
+import { render } from '@testing-library/react'
+import { FavoriteNumber } from '../favorite-number'
+
+// Reusable function to render the ui and get queries 
+// This whole function is already been implemented into RTL
+// and has a name as render function
+
+// function render(ui) {
+//   const container = document.createElement('div')
+//   ReactDOM.render(ui, container)
+//   const queries = getQueriesForElement(container)
+//   return { container, ...queries }
+// }
 
 test('renders a number input with a label "Favorite Number"', () => {
-  const div = document.createElement('div')
-  ReactDOM.render(<FavoriteNumber />, div)
-  expect(div.querySelector('input').type).toBe('number')
-  expect(div.querySelector('label')).toHaveTextContent('Favorite Number')
+  const { getByLabelText } = render(<FavoriteNumber />);
+  const input = getByLabelText(/Favorite Number/i);
+  expect(input).toHaveAttribute('type', 'number')
 })
